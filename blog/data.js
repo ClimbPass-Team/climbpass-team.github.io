f49cd039a5dbc5070b5494b06b8e4913a03eff00
Clemence Goh <clemencegoh@users.noreply.github.com>
2020-07-14 17:51:35 +0800

    just trying

--- blog/data.js
+++ blog/data.js
@@ -1,12 +1,12 @@
 var blogData = [
     {
-        "src": "images/placeholder_climb.jpg",
+        "src": "../images/placeholder_climb.jpg",
         "alt": "First post",
         "title": "My First Post",
         "description": "My first post on this site! So excited to share"
     },
     {
-        "src": "images/placeholder_climb.jpg",
+        "src": "../images/placeholder_climb.jpg",
         "alt": "First post",
         "title": "Placeholder 2",
         "description": "something something great mate"
